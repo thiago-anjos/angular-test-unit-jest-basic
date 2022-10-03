02187f4b5489dd7e5975e56de5a9f50aadda18f9
thiago-anjos <your_email@example.com>
2022-10-02 23:26:15 -0300

    test routes

---
+++ book_tests_class/src/app/nav/nav.component.spec.ts
@@ -0,0 +1,52 @@
+import { NavComponent } from './nav.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
+class ComponentTestRoute {}
+
+describe('Nav component', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: 'home',
+            component: ComponentTestRoute,
+          },
+          {
+            path: 'cart',
+            component: ComponentTestRoute,
+          },
+        ]),
+      ],
+      declarations: [NavComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('navTo', () => {
+    const router = TestBed.inject(Router);
+    const spy = jest.spyOn(router, 'navigate');
+
+    component.navTo('home');
+    expect(spy).toHaveBeenCalledWith(['/home']);
+
+    component.navTo('cart');
+    expect(spy).toHaveBeenCalledWith(['/cart']);
+  });
+});
